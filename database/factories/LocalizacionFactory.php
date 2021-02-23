e6499e09ff47a0fb26e69365ce00745357874746
Bilal <you@example.com>
2021-02-23 19:45:46 +0100

    LP1-17: Creadas las migraciones, los modelos y las factorias

---
+++ database/factories/LocalizacionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Localizacion;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LocalizacionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Localizacion::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
