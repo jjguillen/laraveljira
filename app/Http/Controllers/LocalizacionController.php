1da299d58f9094ea4cb68f336f27f6b5115eda97
Rasma <rasmabu@gmail.com>
2021-02-28 23:39:28 +0100

    LP1-7 LocalizacionController creado

---
+++ app/Http/Controllers/LocalizacionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Localizacion;
+use Illuminate\Http\Request;
+
+class LocalizacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $localizaciones = Localizacion::paginate(3);
+        return view('localizacion.listado', ['localizaciones' => $localizaciones]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Localizacion  $localizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Localizacion $localizacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Localizacion  $localizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Localizacion $localizacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Localizacion  $localizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Localizacion $localizacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Localizacion  $localizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Localizacion $localizacion)
+    {
+        //
+    }
+}
