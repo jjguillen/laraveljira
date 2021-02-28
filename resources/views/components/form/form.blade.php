31240218b57b04113d7c6c72a8f3ea4bb08ca8ff
Rasma <rasmabu@gmail.com>
2021-02-28 19:50:18 +0100

    LP1-3 Updated form component style

--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-6">
+<div class="bg-gray-100 flex flex-col justify-center sm:py-6">
     <div class="relative sm:max-w-xl sm:mx-auto ">
         <div class="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
             <form class="max-w-md mx-auto" method="POST" action="{{ $action }}" enctype="multipart/form-data">
