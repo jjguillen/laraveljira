bcf6f8f26b511c9e4a03adc14d610eeea66944d5
Rasma <rasmabu@gmail.com>
2021-02-28 22:54:27 +0100

    LP1-3 Añadido el atributo ´value´ para componentes de formulario

--- app/View/Components/Form/InputNumber.php
+++ app/View/Components/Form/InputNumber.php
@@ -7,16 +7,19 @@ use Illuminate\View\Component;
 class InputNumber extends Component
 {
     public $name;
+    public $value;
 
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct($name)
+    public function __construct($name = "", $value = "")
     {
         $this->name = $name;
+        $this->value = $value;
     }
+
 
     /**
      * Get the view / contents that represent the component.
--- resources/views/components/form/input-select.blade.php
+++ resources/views/components/form/input-select.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col flex-1">
     <label class="leading-loose">{{ $slot }}</label>
-    <select name="{{ $name }}" id="{{ $name }}"
+    <select name="{{ $name }}" id="{{ $name }}" value="{{ $value }}"
         class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
         {{ $options }}
     </select>
