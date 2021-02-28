e5153db243bb6472623caa872187ab2a849b7aaa
Eduard Stefan <eduardstefan290@gmail.com>
2021-02-28 15:48:03 +0100

    LP1-10 eliminó el controlador de producto
    He añadido accidentalmente el controlador con la rama
    que debe ser sólo para la vista.
    Así que lo estoy eliminando y lo añadiré en la rama para
    el controlador.

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -5,18 +5,3 @@ namespace App\Http\Controllers;
 use App\Models\Localizacion;
 use App\Models\Producto;
 use Illuminate\Http\Request;
-
-class ProductoController extends Controller
-{
-    public function index()
-    {
-        return view('productos', [
-            'productos' => Producto::all()
-        ]);
-    }
-
-    public function localizacion()
-    {
-        return $this->belongsTo(Localizacion::class, 'index');
-    }
-}
