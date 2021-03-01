92f30f85bbb994da58a6624dd4389484ef985bfc
Bilal <you@example.com>
2021-03-01 20:31:13 +0100

    LP1-23 Creadas las rutas

--- routes/web.php
+++ routes/web.php
@@ -29,20 +29,22 @@ Route::get('/admin', function () {
 
 Route::prefix('admin')->middleware('auth')->group(function () {
 
+    // Rutas para categorias
     Route::group(['prefix' => 'categorias'], function () {
         Route::resource('/', CategoriaController::class)->except('edit');
     });
 
+    // Rutas para productos
     Route::group(['prefix' => 'productos'], function () {
         Route::resource('/', ProductoController::class);
-        Route::get('/localizacionAsignada', [ProductoController::class, 'localizacionAsignada']);
-        Route::get('/asignarLocalizacion', [ProductoController::class, 'asignarLocalizacion']);
     });
 
+    // Rutas para localizaciones
     Route::group(['prefix' => 'localizaciones'], function () {
         Route::resource('/', LocalizacionController::class)->except('edit');
     });
 
+    // Ruta para el inventario
     Route::get('/inventario', function () {
         return view('admin.inventario');
     });
