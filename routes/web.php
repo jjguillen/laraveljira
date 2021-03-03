6e49314a93a639ae802698a7a01d9559d9eb1113
Bilal <you@example.com>
2021-03-03 12:21:12 +0100

    LP1-23 Modificaciones en las rutas(He borrado las que no vamos a usar)

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\LocalizacionController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Aquí están las rutas del proyecto
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
 });
 
+// Web principal
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-//Aquí las rutas nuevas
-Route::get('/admin', function () {
-    return "Bienvenido a admin";
-});
-
-
+// Administración
 Route::prefix('admin')->middleware('auth')->group(function () {
 
     // Rutas para categorias
