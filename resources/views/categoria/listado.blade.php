96e0e02f4d5184c74b43cb40dcfb230f0e431924
IsmaelF01 <ismaelfloressanchez@gmail.com>
2021-03-04 12:06:16 +0100

    LP1-3 creacion y finalizacion de las vistas de categoria

---
+++ resources/views/categoria/listado.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-table.table resource-name="categorias">
+        <x-table.head>
+            <x-table.head-cell>ID</x-table.head-cell>
+            <x-table.head-cell>Nombre</x-table.head-cell>
+        </x-table.head>
+        <x-table.body>
+            @foreach ($categorias as $categoria)
+                <x-table.row resource-name="categorias" :resource-id="$categoria->id">
+                    <x-table.cell>{{ $categoria->id }}</x-table.cell>
+                    <x-table.cell>{{ $categoria->nombre }}</x-table.cell>
+                </x-table.row>
+            @endforeach
+        </x-table.body>
+    </x-table.table>
+</x-app-layout>
---
+++ resources/views/categoria/nuevo.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-form.form action="/admin/categorias">
+      <x-form.header>Categorias</x-form.header>
+      <x-form.content route-for-cancel="/admin/categorias">
+        <x-form.form-group>
+          <x-form.input-text name="categoria">Categoria</x-form.input-text>
+        </x-form.form-group>
+      </x-form.content>
+    </x-form.form>
+</x-app-layout>
